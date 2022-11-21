a905f29d7fe5843236883262a85d71cf01714e39
Talee Y <talee.yng@gmail.com>
2022-11-21 01:36:24 -0600

    Add code to App.js, PortfolioContainer.js, and Footer.js file

---
+++ my-portfolio-app/src/components/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default function Footer() {
+    return (
+        <div class="personal-info">
+        <ul>
+            <li>
+                <a href="https://github.com/tyang896">LinkedIn</a>
+            </li>
+            <li>
+                <a href="https://github.com/tyang896">GitHub</a>
+            </li>
+            <li>
+                <a href="mailto:talee.yng@gmail.com">talee.yng@gmail.com</a>
+            </li>
+            <li>
+                <a href="tel:+19526889231">952.688.9231</a>
+            </li>
+        </ul>
+    </div>
+    )
+}
