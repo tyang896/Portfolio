b500438fcddb90e9d5b7094dd004beac56a99edc
Talee Y <talee.yng@gmail.com>
2022-12-29 20:17:26 -0600

    Change styling for about me description

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,7 +5,7 @@ export default function About() {
     <main className="p-3">
       <h1 className="title">About Me</h1>
       <div id="profile-pic"></div>
-      <p>
+      <p className="about-me-desc">
       Hello! My name is Talee Yang and I am currently attending the University of Minnesota coding bootcamp. In the past I have worked in the bio-pharmaceuticals industry as a manufacturing associate. I am very passionate about learning and I have an interest in coding and design. Once I graduate from the coding bootcamp, I look forward to starting a new career as a web developer.
       </p>
     </main>
