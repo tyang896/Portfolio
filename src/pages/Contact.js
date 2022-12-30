4e498dd0d4a532eab1a54d28e98ec0f1431195f4
Talee Y <talee.yng@gmail.com>
2022-12-29 21:37:25 -0600

    Add helper method and error messages for contact form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,22 +1,91 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { validateEmail } from '../utils/helpers'
+
 
 export default function Contact() {
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+  const [message, setMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleInputChange = (event) => {
+    const { target } = event;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    if (inputType === 'name'){
+      setName(inputValue);
+    } else if (inputType === 'email') {
+      setEmail(inputValue);
+    } else {
+      setMessage(inputValue);
+    }
+  }
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    if(!name){
+      setErrorMessage('Name cannot be left blank')
+      return;
+    }
+
+    // If email address is invalid, display error message
+    if (!validateEmail(email)){
+      setErrorMessage('Email is invalid');
+      return;
+    }
+
+    if (!message) {
+      setErrorMessage('Message cannot be left blank');
+      return;
+    }
+
+    //clears the input after successful submission
+    setName('')
+    setEmail('');
+    setMessage('');
+  }
+
   return (
     <main className="p-3">
       <h1 className="title">Contact</h1>
       <form className="form">
         <p>Name:</p>
-        <input name="name" type="name" />
+        <input 
+        name="name" 
+        type="name" 
+        placeholder="name"
+        value={name}
+        onChange={handleInputChange}
+        />
         <br />
         <p>Email Address:</p>
-        <input name="email" type="email" />
+        <input 
+        name="email" 
+        type="email"
+        placeholder="email"
+        value={email}
+        onChange={handleInputChange}
+         />
         <br />
         <p>Message: </p>
-        <textarea name="message" type="message"></textarea>
+        <textarea 
+        name="message" 
+        type="message"
+        placeholder="message"
+        value={message}
+        onChange={handleInputChange}>
+        </textarea>
         <div>
-          <button type="button" className="btn btn-primary" onClick={""}>Submit</button>
+          <button type="button" className="btn btn-primary" onClick={handleFormSubmit}>Submit</button>
         </div>
       </form>
+      {errorMessage && (
+        <div>
+          <p className="error-text">{errorMessage}</p>
+        </div>
+      )}
     </main>
   );
 }
