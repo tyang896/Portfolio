c8f6a5f95ea8b7b50b31b8c499f2c70c70bcc2cf
Talee Y <talee.yng@gmail.com>
2023-03-01 02:07:46 -0600

    Add Linkedin profile

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,8 +5,7 @@ export default function Footer() {
         <div className="personal-info footer">
         <ul>
             <li>
-                {/* TODO: Add a link to LinkedIn profile */}
-                <a href="https://github.com/tyang896">LinkedIn</a>
+                <a href="https://linkedin.com/in/talee-yang-49293aa5">LinkedIn</a>
             </li>
             <li>
                 <a href="https://github.com/tyang896">GitHub</a>
