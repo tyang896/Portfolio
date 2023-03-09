3542e1b819e76f935f4c1c71456d697885de4b9c
Talee Y <talee.yng@gmail.com>
2023-03-09 05:55:13 -0600

    Add functionality to open sidebar menu for mobile devices

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,16 +3,28 @@ import Navigation from './Navigation'
 
 //handlePageChange and currentPage are being destructured from PortfolioContainer.js
 export default function Header({ handlePageChange, currentPage }) {
+    const openNav = () => {
+        document.getElementById("mySidebar").style.width = "250px";
+    }
+
+    const closeNav = () => {
+        document.getElementById("mySidebar").style.width = "0";
+    }
+
     return (
         <header >
             {/* Display this layout if user was on mobile devices */}
             <div className="sm-device">
+                <div id="mySidebar" class="sidebar">
+                    <button class="closebtn" onClick={closeNav}>&times;</button>
+                    <Navigation handlePageChange={handlePageChange} currentPage={currentPage} />
+                </div>
                 <div className="p-2 text-center row">
                     <div className="d-inline text-start col">
-                        <button className="openbtn" onclick="">&#9776;</button>
+                        <button className="openbtn" onClick={openNav}>&#9776;</button>
                     </div>
-                    <div className="col-10">
-                        <h2 className="text-light text-end me-3">Talee Yang</h2>
+                    <div className="col-8">
+                        <h2 className="text-light text-end me-3 pt-2">Talee Yang</h2>
                     </div>
                 </div>
             </div>
