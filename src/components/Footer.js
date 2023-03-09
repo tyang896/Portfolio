62afd6ff82b4c644e14ef88a02474302acbb6718
Talee Y <talee.yng@gmail.com>
2023-03-09 00:33:37 -0600

    Add code to change navbar layout on mobile devices

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,16 +5,16 @@ export default function Footer() {
         <div className="personal-info footer">
         <ul>
             <li>
-                <a href="https://linkedin.com/in/talee-yang-49293aa5" target="_blank" rel="noreferrer noopener">LinkedIn</a>
+                <a href="https://linkedin.com/in/talee-yang-49293aa5" target="_blank" rel="noreferrer noopener" className="footer-link">LinkedIn</a>
             </li>
             <li>
-                <a href="https://github.com/tyang896" target="_blank" rel="noreferrer noopener">GitHub</a>
+                <a href="https://github.com/tyang896" target="_blank" rel="noreferrer noopener" className="footer-link">GitHub</a>
             </li>
             <li>
-                <a href="mailto:talee.yng@gmail.com">talee.yng@gmail.com</a>
+                <a href="mailto:talee.yng@gmail.com" className="footer-link">talee.yng@gmail.com</a>
             </li>
             <li>
-                <a href="tel:+19526889231">952.688.9231</a>
+                <a href="tel:+19526889231" className="footer-link">952.688.9231</a>
             </li>
         </ul>
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,27 @@ import React from 'react';
 import Navigation from './Navigation'
 
 //handlePageChange and currentPage are being destructured from PortfolioContainer.js
-export default function Header({handlePageChange, currentPage}){
+export default function Header({ handlePageChange, currentPage }) {
     return (
-        <header>
-            <h1 className='title-name p-3'>
-                Talee Yang
-            </h1>
-            <Navigation handlePageChange={handlePageChange} currentPage={currentPage}/>
+        <header >
+            {/* Display this layout if user was on mobile devices */}
+            <div className="sm-device">
+                <div className="p-2 text-center row">
+                    <div className="d-inline text-start col">
+                        <button className="openbtn" onclick="">&#9776;</button>
+                    </div>
+                    <div className="col-10">
+                        <h2 className="text-light text-end me-3">Talee Yang</h2>
+                    </div>
+                </div>
+            </div>
+            {/* Display this layout if user is on a tablet or laptop */}
+            <div className="lg-device">
+                <h1 className='title-name p-3'>
+                    Talee Yang
+                </h1>
+                <Navigation handlePageChange={handlePageChange} currentPage={currentPage} />
+            </div>
         </header>
     )
 }
